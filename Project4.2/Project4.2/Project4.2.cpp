dff09cc66bc8bf6ebf9921a4d4bd4bf8250560ed
Tomi933 <strashivskaoksana@gmail.com>
2024-10-12 16:04:33 +0300

    Add files via upload

---
+++ Project4.2/Project4.2/Project4.2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = (x * x) / 2,1 + sin(abs(x));
+		if (x <= -5)
+			B = 1.0/tan(exp(x));
+		else
+			if (x >= 0)
+				B = log10(sqrt(abs(x) - (x * x) / 2));
+			else
+				B = 2 - pow(x, 3) / (abs(x) + 1);
+		y = A + B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
